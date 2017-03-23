2d1a0d60a4084a93f764a7d1d53b40e504f336ef
Larry D. Cherry <larry.cherry.0406@gmail.com>
2017-03-22 23:23:43 -0500

    mapped keys a-f with tones

--- js/tone.js
+++ js/tone.js
@@ -3,12 +3,43 @@ var synth = new Tone.Synth().toMaster();
 $(document).ready(function() {
 	down = {}
   $('body').on( "keydown", function(e) {
-	if (down['e.which'] == null)
-	console.log(e.which)
-	down['e.which'] = true
+	// if (down['e.which'] == null)
+	// console.log(e.which)
+	// down['e.which'] = true
+ 	if (e.which == 65){
+ 	  synth.triggerAttackRelease('A3', '1n')
+ 	}
+ 	else if (e.which == 83) {
+ 	  synth.triggerAttackRelease('B3', '1n')
+ 	}
+ 	else if (e.which == 68) {
+ 	  synth.triggerAttackRelease('C3', '1n')
+ 	}
+ 	else if (e.which == 70) {
+ 	  synth.triggerAttackRelease('D3', '1n')
+ 	}
+ 	else if (e.which == 71) {
+ 	  synth.triggerAttackRelease('E3', '1n')
+ 	}
   })
+  	
 
   $('body').on( "keyup", function(e) {
-  	down['e.which'] = null
+  	//down['e.which'] = null
+  	if (e.which == 65){
+  	  synth.triggerRelease()
+  	}
+  	else if (e.which == 83) {
+  	 synth.triggerRelease()
+  	}
+  	else if (e.which == 68) {
+  	  synth.triggerRelease()
+  	}
+  	else if (e.which == 70) {
+  	  synth.triggerRelease()
+  	}
+  	else if (e.which == 71) {
+  	  synth.triggerRelease()
+  	}
   })	
 }) 
